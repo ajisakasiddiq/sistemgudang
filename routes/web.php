867000c489f0c74bb0e9b72c4ea0cc39154a97c0
AJISAKA SIDDIQ <ajisakasiddiq@gmail.com>
2024-08-21 11:10:11 +0700

    update tampilan

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware();
+Route::get('/Login', function () {
+    return view('pages.sign-in');
+});
+Route::get('/Register', function () {
+    return view('pages.sign-up');
 });
